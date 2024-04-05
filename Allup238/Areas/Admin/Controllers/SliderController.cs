8a0fd0fdbcf221ad23c154760ccb0107168e9d3b
SeymurAliyeev <seymuraa@code.edu.az>
2024-04-05 13:16:10 +0400

    ArchitectureWasBuiltAndEssentialPartsWereCreated

---
+++ Allup238/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AllupP238.Areas.Admin.Controllers
+{
+    public class SliderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
